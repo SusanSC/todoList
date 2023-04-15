40fb6489c873c2d7f326ec776253b6d56aa759fd
Susan Segura <susansegura26@gmail.com>
2023-04-15 15:05:56 -0600

    Add gitignore and update the ports

--- app.js
+++ app.js
@@ -215,6 +215,6 @@ app.get("/about", function(req, res){
 
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000");
 });
